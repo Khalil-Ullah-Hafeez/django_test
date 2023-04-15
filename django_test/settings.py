c792951746759025906c513a079ecb6e8210fefd
Khalil-Ullah-Hafeez <khalil.formal@gmail.com>
2023-04-15 11:55:25 +0500

    FOLLOWED video

--- django_test/settings.py
+++ django_test/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^mf9i7q1y8sbya@xgxnse4ph@df2-dhbg&7sc0spofzltq-b_i'
+SECRET_KEY = 'django-insecure-r4#dhb0agry)!-8z3#w(6^az%8f^ecaj4z=vay52d6&+499a3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,11 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
